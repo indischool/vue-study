59e678665d7413043d88c50d11ced0948038c53a
jdssem <jdssem@gmail.com>
2019-05-15 14:37:30 +0900

    8. Components Within Components

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks" v-text="task.description"></task>
+        </div>
+    `,
+
+    data() {
+        return {
+            tasks: [{
+                    description: 'Go to the store',
+                    completed: true
+                },
+                {
+                    description: 'Go to the email',
+                    completed: false
+                },
+                {
+                    description: 'Go to the farm',
+                    completed: true
+                },
+                {
+                    description: 'Go to work',
+                    completed: false
+                },
+            ]
+        }
+    }
+})
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
